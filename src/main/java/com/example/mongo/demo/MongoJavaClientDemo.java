92d6dbd7c45650ddfe114640dcd61a9b5bda9898
raj <torajakumar@gmail.com>
2019-07-20 22:22:35 +0800

    insert java pojo using mongo client

---
+++ src/main/java/com/example/mongo/demo/MongoJavaClientDemo.java
@@ -0,0 +1,9 @@
+package com.example.mongo.demo;
+
+import com.mongodb.client.MongoClient;
+
+public class MongoJavaClientDemo {
+    public static void main(String[] args) {
+        MongoClient
+    }
+}
